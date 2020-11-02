7584e85ccacc48eaff6918fb698313a8acf3e8be
MikuroXina <ryosukadnak@gmail.com>
2020-11-02 20:16:20 +0900

    🔥 Prune files

--- project/Assets/scripts/sceneChange/SceneChangeToForest.cs
+++ project/Assets/scripts/sceneChange/SceneChangeToForest.cs
@@ -6,23 +6,23 @@ using UnityEngine;
 public class SceneChangeToForest : MonoBehaviour
 {
     public string SceneName = "";
-    private VRTK.VRTK_InteractableObject vrtk_InteractableObject = new VRTK.VRTK_InteractableObject();
+    // private VRTK.VRTK_InteractableObject vrtk_InteractableObject = new VRTK.VRTK_InteractableObject();
     private float GripTime;
     readonly float DecideGripTime = 3.0f;
 
     private void Start()
     {
-        vrtk_InteractableObject = this.gameObject.GetComponent<VRTK.VRTK_InteractableObject>();
-        vrtk_InteractableObject.isGrabbable = true;
+        // vrtk_InteractableObject = this.gameObject.GetComponent<VRTK.VRTK_InteractableObject>();
+        // vrtk_InteractableObject.isGrabbable = true;
     }
 
     private void Update()
     {
-        if (vrtk_InteractableObject.isGrabbable)
-        {
-            GripTime = 0;
-        }
-        else
+        // if (vrtk_InteractableObject.isGrabbable)
+        // {
+        //     GripTime = 0;
+        // }
+        // else
         {
             GripTimeCounter();
         }
